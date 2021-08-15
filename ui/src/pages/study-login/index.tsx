7609e380781cf1dc7e6bd99f19fe08bda800021f
Zachary Ng <zacharyngweiyao@gmail.com>
2021-08-15 20:34:56 +1000

    Fix build error

--- ui/src/pages/study-login/index.tsx
+++ ui/src/pages/study-login/index.tsx
@@ -17,7 +17,7 @@ export function StudyLoginScreen() {
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Register for Research Study</h2>
                     <p className="mt-2 text-center text-sm text-gray-600">
                         Logging in here gives consent to {''}
-                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                        <a className="font-medium text-indigo-600 hover:text-indigo-500">
                             Terms and Conditions
                         </a>
                     </p>
